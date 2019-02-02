ba5606ee1b8af4a7085092ebe540fb8d8954b354
Helmut Rhomberg <hrh5775@students.fhv.at>
2019-02-02 21:53:10 +0100

    added generated spring boot code

---
+++ src/test/java/at/teamhsl/random_image_generator/RandomImageGeneratorApplicationTests.java
@@ -0,0 +1,17 @@
+package at.teamhsl.random_image_generator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RandomImageGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
