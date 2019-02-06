63666dee6b3f636785563ca78ec978b8bb121701
Helmut Rhoberg <helmut_rhomberg@gmx.at>
2019-02-06 15:53:26 +0100

    added new test key

--- src/main/java/at/teamhsl/random_image_generator/Helpers/RandomImageURLHelper.java
+++ src/main/java/at/teamhsl/random_image_generator/Helpers/RandomImageURLHelper.java
@@ -46,7 +46,7 @@ public class RandomImageURLHelper {
 		// Start of user code GetRandomImageURL
 		RandomImageURLHelper instance = getInstance();
 		instance.APIURL = "https://www.googleapis.com/customsearch/v1?q=" + URLEncoder.encode(Word, "UTF-8") + "&cx=000784618626346885213%3Ar14xhqlh_xc&imgSize=xlarge&num=1&searchType=image&key=";
-		INSTANCE.APIKEY = "AIzaSyBNK9hZY6VlQqY3O-XH50pqsF8mnSV1AI4";
+		INSTANCE.APIKEY = "AIzaSyBLGXOnmZSotzM_DZTdq5hD486Qy5IXDMo";
 		//INSTANCE.APIURL = "https://www.googleapis.com/customsearch/v1?q=cat&cx=000784618626346885213%3Ar14xhqlh_xc&imgSize=xlarge&num=1&searchType=image&key=";
 		URL url = new URL(instance.APIURL + instance.APIKEY);
 		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
